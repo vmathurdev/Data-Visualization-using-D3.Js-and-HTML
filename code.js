5aee4e2df5a15fe4b72dabeea90c6daf4d740e6c
Vinamra Mathur <vinamra.mathur@hotmail.co.in>
2020-11-09 02:09:38 +0530

    Create code.js

---
+++ code.js
@@ -0,0 +1,44 @@
+var data = [
+  {
+    "title": "LARGE DOG",
+    "footprint": 1.1,
+    "subtitle": "",
+    "subtitle2": "",
+    "subtitle3": ""
+  },
+  {
+    "title": "MEDIUM-SIZED DOG",
+    "footprint": 0.84,
+    "subtitle": "CONSUMPTION PER YEAR",
+    "subtitle2": "164kg of meat, 95kg or cereals",
+    "subtitle3": "43.3m2 of land per 1kg of chicken (more for beef and lamb), 13.4m2 of land per 1kg of cereals"
+  },
+  {
+    "title": "TOYOTA LAND CRUISER",
+    "footprint": 0.41,
+    "subtitle": "10,000km DRIVEN PER YEAR",
+    "subtitle2": "55.1 gigajoules (includes energy required to fuel and construct)",
+    "subtitle3": ""
+  },
+  {
+    "title": "VOLKSWAGEN GOLF",
+    "footprint": 0.18,
+    "subtitle": "",
+    "subtitle2": "",
+    "subtitle3": ""
+  },
+  {
+    "title": "CAT",
+    "footprint": 0.15,
+    "subtitle": "",
+    "subtitle2": "",
+    "subtitle3": ""
+  },
+  {
+    "title": "HAMSTER",
+    "footprint": 0.014,
+    "subtitle": "",
+    "subtitle2": "",
+    "subtitle3": ""
+  }
+]
